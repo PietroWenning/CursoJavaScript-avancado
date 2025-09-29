e7d1aee10a82c9f8dac9216db60661fcca9ee7ff
Pietro calebe <pietrorepositorio@gmail.com>
2025-09-29 12:50:18 -0300

    metodos de objetos

---
+++ Sessao6Objetos/Aula68/index.js
@@ -0,0 +1,6 @@
+const produto ={ nome:'produto', preco:1.8}
+Object.defineProperty(produto, 'nome',{
+    writable:false,
+    configurable:false
+})
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome'))
