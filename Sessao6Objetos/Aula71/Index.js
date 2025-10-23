168bb9ed2fbb8f4ac67c756ad2a5e5c2286d6e5c
Pietro calebe <pietrorepositorio@gmail.com>
2025-10-23 17:15:18 -0300

    .

--- Sessao6Objetos/Aula71/Index.js
+++ Sessao6Objetos/Aula71/Index.js
@@ -4,10 +4,10 @@ function Produto(nome, preco) {
     this.preco = preco;
 }
 Produto.prototype.aumento = function(quantia){
-    this.preco =+ quantia;
+    this.preco += quantia;
 }
-Produto.prototyope.desconto = function(quantia){
-    this.preco =- quantia;
+Produto.prototype.desconto = function(quantia){
+    this.preco -= quantia;
 }
 function  Camiseta(nome, preco, cor){
     Produto.call(this, nome, preco);
@@ -15,13 +15,36 @@ function  Camiseta(nome, preco, cor){
 }
 Camiseta.prototype = Object.create(Produto.prototype);
 Camiseta.prototype.constructor = Camiseta;
+
 Camiseta.prototype.aumento = function(percentual){
     this.preco = this.preco + (this.preco * (percentual / 100));
 }
+function Caneca(nome, preco, material){
+    Produto.call(this, nome, preco);
+    this.material = material;
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function(){
+            return this._estoque;
+        }
+        ,
+        set: function(valor){
+            if (typeof valor !== 'number') return;
+            this._estoque = valor;
+        }
+    });
+}
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+Caneca.prototype.aumento = function(percentual){
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
+const caneca= new Caneca('Caneca de Porcelana', 15, 'Porcelana');
+caneca.aumento(10);
+console.log(caneca);
  const produto = new Produto('Genérica', 50);
- console.log(produto);
  const camiseta1 = new Camiseta('Regata', 20, 'Preta');
-
  camiseta1.aumento(10);
-
  console.log(camiseta1);
